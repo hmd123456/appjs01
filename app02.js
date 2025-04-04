bdf192e7915eb4ec2d1044549854dfb7e7278ba3
Mahesh Matsendranath Deglurkar <MDeglurkar@delta.kaplaninc.com>
2025-04-04 18:39:22 +0100

    Initial commit

--- app02.js
+++ app02.js
@@ -11,9 +11,9 @@ const app = express();
 // MSAL Configuration (Replace placeholders with your actual values)
 const msalConfig = {
     auth: {
-        clientId: '9967', // Replace with your Azure AD App's Client ID
+        clientId: '4de3bcc88e2-9967-4de9-b428-b6ab48db19ae9', // Replace with your Azure AD App's Client ID
         authority: 'https://login.microsoftonline.com/5558459a-5e38-45de-8742-ec475127560c', // Replace with your Tenant ID
-        clientSecret: '2a051337' // Replace with your Azure AD App's Client Secret
+        clientSecret: '2a052a051337-3580-4f04-be0b-5b58d9d0dc73' // Replace with your Azure AD App's Client Secret
     },
     system: {
         loggerOptions: {
